a115820f207a7d16b1b676eb3a87b31edc68b145
petesaar <pete.saarela@pp.inet.fi>
2014-09-06 12:36:34 +0300

    lisätty nb-kamaa

---
+++ testi.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+  $tervehdys = "tässolis ny php-ohjelma";
+  $ja = "1234567!"#¤%"; 
+?><!DOCTYPE HTML>
+<html>
+<head><title><?php echo $tervehdys; ?></title></head>
+<body>
+  <h1><?php echo $tervehdys; ?></h1>
+  <?php echo $ja; ?>
+</body>
+</html>
